47afaf0bbae44b1756183590f7db07347b5c1e44
Durga <121283320+getyourdurga@users.noreply.github.com>
2023-04-24 20:57:36 +0530

    changed script.sh

--- script/first_script.sh
+++ script/first_script.sh
@@ -4,5 +4,3 @@ echo "Enter some runtime variable"
 read runtimevar
 echo "below is the value that has entered"
 echo "run time value is $runtimevar"
-#awk -F: '{ print $1 }' /etc/passwd > /home/ec2-user/scripts/users
-something changed
